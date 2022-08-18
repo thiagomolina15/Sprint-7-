b4a69c0708bc1bca4689d69db1ed8d9628e9b505
thiago_ <105317506+thiagomolina15@users.noreply.github.com>
2022-08-18 19:30:40 -0300

    Delete Login directory

--- Login/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class nuevoUsuario(models.Model):
-    nombreUsuario=models.CharField(max_length=10)
-    contraseña=models.CharField(max_length=10)
-    
--- Login/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def login(request):
-    return render(request, "login/login.html")
-
-def registro(request):
-        return render(request, "login/registro.html")
